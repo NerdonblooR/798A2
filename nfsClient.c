c09b93a100c52a373237e2cf9165030da6e5a781
NerdonblooR <njbbtan@gmail.com>
2017-02-21 14:34:04 -0500

    able to open on server

--- nfsClient.c
+++ nfsClient.c
@@ -1,23 +1,33 @@
-#include "nfsClient.h"
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <stdio.h>
 
+
+#define SERV_PORT 9876
+
+#define INITIAL_SIZE 100
 #define BUFFER_SIZE 4096
-#define SERV_PORT 3000
-
 
 #define CRT_DIR_FLAG   1
 #define CRT_FILE_FLAG  2
 #define OPEN_DIR_FLAG  3
 #define OPEN_FILE_FLAG 4
-
-
 #define READ_CALL_ID 5
 #define WRITE_CALL_ID 6
 #define FILE_SYNC_CALL_ID 7
 #define READ_DIR_CALL_ID 8
 #define RM_DIR_CALL_ID 9
 
-#define htonll(x) ((1==htonl(1)) ? (x) : ((uint64_t)htonl((x) & 0xFFFFFFFF) << 32) | htonl((x) >> 32))
-#define ntohll(x) ((1==ntohl(1)) ? (x) : ((uint64_t)ntohl((x) & 0xFFFFFFFF) << 32) | ntohl((x) >> 32))
+
+typedef struct file_handler {
+    /* data */
+    int file_id;
+    size_t offset;
+    char *wc;
+} file_handler;
 
 typedef struct my_buffer {
     size_t next;
@@ -34,7 +44,7 @@ my_buffer *new_buffer() {
 }
 
 void reserve_space(my_buffer *b, size_t bytes) {
-    if ((b->next + bytes) > b->size) {
+    while ((b->next + bytes) > b->size) {
         /* double size to enforce O(lg N) reallocs */
         b->data = realloc(b->data, b->size * 2);
         b->size *= 2;
@@ -49,270 +59,219 @@ void serialize_size_t(size_t x, my_buffer *b) {
     b->next += sizeof(x);
 }
 
-void serialize_str(char *x, my_buffer *b) {
+
+void serialize_int(int x, my_buffer *b) {
+    x = htonl(x);
+    reserve_space(b, sizeof(x));
+    memcpy(b->data + b->next, &x, sizeof(x));
+    b->next += sizeof(x);
+}
+
+void serialize_char(char x, my_buffer *b) {
+    reserve_space(b, sizeof(x));
+    memcpy(b->data + b->next, &x, sizeof(x));
+    printf("%s\n", b->data + b->next);
+    b->next += sizeof(x);
+}
+
+
+void serialize_str(const char *x, my_buffer *b) {
     reserve_space(b, strlen(x) + 1);
     strcpy(b->data + b->next, x);
+
+    printf("%s\n", b->data + b->next);
     b->next += strlen(x) + 1;
 }
 
 //deserialize str from next
-void deserialize_size_t(size_t *x, char *b, size_t offset) {
-    memcpy(&tmp, b + offset, sizeof(size_t));
-    *x = ntohll(tmp);
+void deserialize_int(int *x, char *b, size_t offset) {
+    int tmp;
+    memcpy(&tmp, b + offset, sizeof(int));
+    *x = ntohl(tmp);
 }
-
 
 //deserialize str from next
-void deserialize_str(char *x, char *b, size_t offset, size_t len) {
-    memcpy(x, b + offset, len);
+char *deserialize_str(char *b, size_t offset) {
+    char *ret = malloc(strlen(b + offset) + 1);//str will be null terminated
+    strcpy(ret, b + offset);
+    printf("ret size: %d\n", strlen(ret));
+    return ret;
 }
 
 
+void get_tcp_response(int sockfd, char *recvbuffer) {
+    char buffer[BUFFER_SIZE];
+    size_t n = recv(sockfd, recvbuffer, BUFFER_SIZE, 0); //initial read
+    if (n == 0) {
+        //error: server terminated prematurely
+        printf("The server terminated prematurely\n");
+        exit(4);
+    }
+
+    printf("received %s\n", recvbuffer);
+    printf("byte recevied %d\n", n);
+
+}
+
+//break large packet into several small one
+void handle_send(int sockfd, my_buffer *arg_buffer) {
+    char sendbuffer[BUFFER_SIZE];
+    memset(sendbuffer, 0, sizeof(sendbuffer));
+    memcpy(sendbuffer, arg_buffer->data, arg_buffer->next);
+    if (arg_buffer->size <= BUFFER_SIZE) {
+        send(sockfd, sendbuffer, arg_buffer->size, 0);
+    } else {
+
+    }
+
+}
+
+void print_buffer(my_buffer *arg_buffer) {
+    printf("size: %d\n", arg_buffer->size);
+    printf("next: %d\n", arg_buffer->next);
+    printf("data: %s\n", arg_buffer->data + 1);
+}
 
 
-typedef struct nfs_context {
-    /* data */
-    //the cache associate with each filre descriptor for commit
+int nfs_open_internal(int sockfd, const char *path, file_handler **fh, const char *command) {
+    //sed a path to server, get a file handler
+    int cid = 1;
+    my_buffer *arg_buffer = new_buffer();
+    serialize_int(cid, arg_buffer);
+    print_buffer(arg_buffer);
+    serialize_str(command, arg_buffer);
+    print_buffer(arg_buffer);
+    serialize_str(path, arg_buffer);
+    print_buffer(arg_buffer);
+    serialize_char('\n', arg_buffer);
+    print_buffer(arg_buffer);
 
-} nfs_context;
+    char recvbuffer[BUFFER_SIZE]; //need to receive a file handler
+    memset(recvbuffer, 0, sizeof(recvbuffer));
 
 
-int get_udp_socket(char *server_ip, struct sockaddr_in *servaddr) {
-    int sockfd;
-    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-        //fail to create socket
+    printf("HERE1\n");
+
+    handle_send(sockfd, arg_buffer);
+
+    printf("HERE2\n");
+
+    get_tcp_response(sockfd, recvbuffer);
+
+    printf("HERE3\n");
+
+    //deserialize the fh received from server
+    //set approporiate field in file_handler
+    file_handler *my_fh = malloc(sizeof(file_handler));
+    int f_id;
+    size_t offset = 0;
+    deserialize_int(&f_id, recvbuffer, offset);
+    offset += sizeof(f_id);
+
+    if (f_id < 0) return -1;
+
+    my_fh->file_id = f_id;
+    printf("file_id: %d\n", my_fh->file_id);
+    my_fh->offset = 0;
+    printf("offset: %d\n", my_fh->offset);
+    my_fh->wc = deserialize_str(recvbuffer,offset);
+    printf("wc: %s\n", my_fh->wc);
+
+
+    *fh = my_fh;
+    free(arg_buffer);
+    return 0;
+}
+
+int nfs_open(int sockfd, const char *path, file_handler **fh) {
+    return nfs_open_internal(sockfd, path, fh, "LOOKUP");
+}
+
+int nfs_create(int sockfd, const char *path, file_handler **fh) {
+    return nfs_open_internal(sockfd, path, fh, "CREATE");
+}
+
+int nfs_mkdir(int sockfd, const char *path) {
+    file_handler **fh;
+    return nfs_open_internal(sockfd, path, fh, "MKDIR");
+}
+
+
+int nfs_rmdir(int sockfd, const char *path) {
+    int cid = 1;
+    my_buffer *arg_buffer = new_buffer();
+    serialize_int(cid, arg_buffer);
+    print_buffer(arg_buffer);
+    serialize_str("DELETE", arg_buffer);
+    print_buffer(arg_buffer);
+    serialize_str(path, arg_buffer);
+    print_buffer(arg_buffer);
+    serialize_char('\n', arg_buffer);
+    print_buffer(arg_buffer);
+
+    char recvbuffer[BUFFER_SIZE]; //need to receive a file handler
+
+    printf("HERE1\n");
+
+    handle_send(sockfd, arg_buffer);
+
+    printf("HERE2\n");
+
+    get_tcp_response(sockfd, recvbuffer);
+
+    printf("HERE3\n");
+
+
+    int response_code;
+    deserialize_int(&response_code, recvbuffer, 0);
+    if (response_code < 0) {
+        printf("delete failed\n");
         return -1;
+    } else {
+        printf("delete success\n");
+
+    }
+    free(arg_buffer);
+    return 0;
+}
+
+
+int main(int argc, char *argv[]) {
+    //communicate with server to set u_id
+
+    if (argc < 2) {
+        printf("No server ip specified\n");
+        exit(1);
+    }
+
+    int sockfd;
+    struct sockaddr_in servaddr;
+
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("Problem in creating the socket\n");
+        exit(2);
     }
 
     //Creation of the socket
-    memset(servaddr,
-           0, sizeof(servaddr));
-    servaddr->sin_family = AF_INET;
-    servaddr->sin_addr.s_addr = inet_addr(server_ip);
-    servaddr->sin_port = htons(SERV_PORT); //convert to big-endian order
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = inet_addr(argv[1]);
+    servaddr.sin_port = htons(SERV_PORT); //convert to big-endian order
 
-    int slen = sizeof(servaddr);
-
-    struct timeval tv;
-    tv.tv_sec = 0;
-    tv.tv_usec = 8000;
-
-    if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv)) < 0) {
-        //fail to set timeout
-        return -1;
-    }
-    return sockfd;
-}
-
-void get_response(int sockfd, char *sendbuffer,  struct sockaddr_in *servaddr, int slen, char *recvbuffer){
-    //send the cd data to server
-    sendto(sockfd, sendbuffer, strlen(sendbuffer), 0, (struct sockaddr *) servaddr, slen);
-
-    //busy waiting for response
-    while (recvfrom(sockfd, recvbuffer, strlen(recvline), 0, (struct sockaddr *) servaddr, &slen) < 0) {
-        //resend
-        sendto(sockfd, sendbuffer, strlen(sendbuffer), 0, (struct sockaddr *) servaddr, slen);
-    }
-}
-
-
-int nfs_open_internal(const char *nfs_server_ip, const char *path, file_handler **fh,  size_t flag) {
-    //sed a path to server, get a file handler
-    struct sockaddr_in servaddr;
-    int sockfd = get_udp_socket(nfs_server_ip, &servaddr);
-    if (sockfd < 0) return -1;
-
-    my_buffer *arg_buffer = new_buffer();
-
-
-    serialize_size_t(flag, arg_buffer);
-    serialize_size_t(strlen(path) + 1, arg_buffer);
-    serialize_str(path, arg_buffer);
-
-    int slen = sizeof(servaddr);
-    char sendbuffer[arg_buffer.next];
-    char recvbuffer[BUFFER_SIZE]; //need to receive a file handler
-    memcpy(sendbuffer, arg_buffer->data, arg_buffer->next);
-
-    get_response(sockfd, sendbuffer,  &servaddr, slen, recvbuffer);
-    //deserialize the fh received from server
-    //set approporiate field in file_handler
-
-    if (flag == OPEN_FILE_FLAG || flag == CRT_CALL_ID){
-        file_handler *my_fh = malloc(sizeof(file_handler));
-        *fh = my_fh;
-    }
-    
-    free(arg_buffer);
-    close(sockfd);
-}
-
-int nfs_open(const char *nfs_server_ip, const char *path, file_handler **fh){
-    return nfs_open_internal(nfs_server_ip, path, fh, OPEN_FILE_FLAG);
-}
-
-int nfs_create(const char *nfs_server_ip, const char *path, file_handler **fh){
-    return nfs_open_internal(nfs_server_ip, path, fh, CRT_FILE_FLAG);
-}
-
-int nfs_open_dir(const char *nfs_server_ip, const char *path){
-    return nfs_open_internal(nfs_server_ip, path, NULL, OPEN_DIR_FLAG);
-}
-
-int nfs_mkdir(const char *nfs_server_ip, const char *path){
-    return nfs_open_internal(nfs_server_ip, path, NULL, CRT_DIR_FLAG);
-}
-
-
-int nfs_read(const char *nfs_server_ip, file_handler *nfsfh, size_t offset, size_t size, char *buf) {
-    //read content from offset, and maximum size fron file specified by the nfsfh
-    struct sockaddr_in servaddr;
-    int sockfd = get_udp_socket(nfs_server_ip, &servaddr);
-    //serialize cd into send sendbuffer;
-    if (sockfd < 0) return -1;
-
-    my_buffer * arg_buffer= new_buffer();
-    serialize_size_t(READ_CALL_ID, arg_buffer);
-    serialize_str(nfsfh->file_id, arg_buffer);
-    serialize_size_t(offset, arg_buffer);
-    serialize_size_t(size, arg_buffer);
-
-
-    int slen = sizeof(servaddr);
-    char sendbuffer[cdbuffer.next];
-    char recvbuffer[BUFFER_SIZE];
-
-    memcpy(sendbuffer, arg_buffer->data, arg_buffer->data);
-
-    get_response(sockfd, sendbuffer,  &servaddr, slen, recvbuffer);
-    //copy data in recvbuffer to buf
-    free(arg_buffer);
-    close(sockfd);
-    return 0;
-}
-
-int nfs_write(const char *nfs_server_ip, file_handler *nfsfh, size_t offset, size_t size, char *buf){
-    struct sockaddr_in servaddr;
-    int sockfd = get_udp_socket(nfs_server_ip, &servaddr);
-    //serialize cd into send sendbuffer;
-    if (sockfd < 0) return -1;
-
-    my_buffer * arg_buffer= new_buffer();
-    serialize_size_t(WRITE_CALL_ID, arg_buffer);
-    serialize_str(nfsfh->file_id, arg_buffer);
-    serialize_str(nfsfh->wc, arg_buffer);
-    serialize_size_t(offset, arg_buffer);
-    serialize_size_t(size, arg_buffer);
-    serialize_size_t(strlen(buf) + 1, arg_buffer);
-    serialize_str(buf, arg_buffer);
-
-
-    int slen = sizeof(servaddr);
-    char sendbuffer[cdbuffer.next];
-    char recvbuffer[BUFFER_SIZE];
-
-    memcpy(sendbuffer, arg_buffer->data, arg_buffer->next);
-
-    get_response(sockfd, sendbuffer,  &servaddr, slen, recvbuffer);
-    //get ack, set wc from sever
-
-
-    free(arg_buffer);
-    close(sockfd);
-    return 0;
-
-}
-
-
-int nfs_fsync(const char *nfs_server_ip, file_handler *nfsfh){
-    //send out call id and file descriptor
-    struct sockaddr_in servaddr;
-    int sockfd = get_udp_socket(nfs_server_ip, &servaddr);
-    //serialize cd into send sendbuffer;
-    if (sockfd < 0) return -1;
-
-    my_buffer * arg_buffer= new_buffer();
-    serialize_size_t(FILE_SYNC_CALL_ID, arg_buffer);
-    serialize_str(nfsfh->file_id, arg_buffer);
-    serialize_str(nfsfh->wc, arg_buffer);
-
-    int slen = sizeof(servaddr);
-    char sendbuffer[cdbuffer.next];
-    char recvbuffer[BUFFER_SIZE];
-
-    memcpy(sendbuffer, arg_buffer->data, arg_buffer->next);
-
-    get_response(sockfd, sendbuffer,  &servaddr, slen, recvbuffer);
-    //get response
-
-    free(arg_buffer);
-    close(sockfd);
-    return 0;
-
-
-    return 0;
-}
-
-
-int nfs_read_dir(const char *nfs_server_ip, const char *path, char ***dirs, int *num_dir){
-    //send out call id and file descriptor
-    struct sockaddr_in servaddr;
-    int sockfd = get_udp_socket(nfs_server_ip, &servaddr);
-    //serialize cd into send sendbuffer;
-    if (sockfd < 0) return -1;
-
-    my_buffer * arg_buffer= new_buffer();
-    serialize_size_t(READ_DIR_CALL_ID, arg_buffer);
-    serialize_size_t(strlen(path) + 1, arg_buffer);
-    serialize_str(path, arg_buffer);
-
-    int slen = sizeof(servaddr);
-    char sendbuffer[cdbuffer.next];
-    char recvbuffer[BUFFER_SIZE];
-
-    memcpy(sendbuffer, arg_buffer->data, arg_buffer->next);
-    get_response(sockfd, sendbuffer,  &servaddrdeserialize_str, slen, recvbuffer);
-    //get response
-
-    int rcv_num_dir = 0;
-    char ** all_dirs = malloc(sizeof(*allDirs) * rcv_num_dir);
-
-    int i = 0;
-    size_t offset =0;
-
-    for (i = 0; i < rcv_num_dir; ++i){
-        int path_len = 0;
-        all_dirs[i] = malloc(sizeof(char) * path_len);
-        deserialize_str(all_dirs[i], recvbuffer, offset, path_len);
+    //Connection of the client to the socket
+    if (connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) {
+        printf("Problem in connecting to the server\n");
+        exit(3);
     }
 
-    *dirs = all_dirs;
+    printf("HERE\n");
 
-    free(arg_buffer);
-    close(sockfd);
+    file_handler *fh;
+    //nfs_create(sockfd, argv[2], &fh);
+    //nfs_open(sockfd, argv[2], &fh);
+    // nfs_mkdir(sockfd, argv[2]);
+    nfs_rmdir(sockfd, argv[2]);
     return 0;
-}
-
-int nfs_rmdir(const char *nfs_server_ip, const char *path){
-    struct sockaddr_in servaddr;
-    int sockfd = get_udp_socket(nfs_server_ip, &servaddr);
-    if (sockfd < 0) return -1;
-
-    my_buffer *arg_buffer = new_buffer();
-
-    serialize_size_t(RM_DIR_CALL_ID, arg_buffer);
-    serialize_size_t(strlen(path) + 1, arg_buffer);
-    serialize_str(path, arg_buffer);
-
-    int slen = sizeof(servaddr);
-    char sendbuffer[arg_buffer.next];
-    char recvbuffer[BUFFER_SIZE]; //need to receive a file handler
-    memcpy(sendbuffer, arg_buffer->data, arg_buffer->next);
-
-    get_response(sockfd, sendbuffer,  &servaddr, slen, recvbuffer);
-    //deserialize the fh received from server
-    free(arg_buffer);
-    close(sockfd);
 }
 
 
