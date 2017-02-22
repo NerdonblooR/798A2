fcd54f99ce9e350a15240811f4cfcad982881107
NerdonblooR <njbbtan@gmail.com>
2017-02-22 14:17:44 -0500

    interface

--- my_fuse_nfs.c
+++ my_fuse_nfs.c
@@ -2,166 +2,170 @@
 #define BUFFER_SIZE 4096
 #define SERV_PORT 3000
 
-
-
 #include <config.h>
 #include <fuse.h>
 #include <fuse_lowlevel.h>
 #include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 #include <string.h>
 #include <unistd.h>
 #include <time.h>
 #include <errno.h>
-#in
 
 
-struct nfs_context *nfs = NULL;
-char *server_ip;
+#define SERV_PORT 3001
+
+
+typedef struct nfs_context {
+    char *server_ip;
+    int port_num;
+    int sock_fd;
+    int is_up; //1 is up, 0 is down.
+} nfs_context;
 
 
 
 static int nfs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
-			 off_t offset, struct fuse_file_info *fi)
-{
-	struct nfsdir *nfsdir;
-	struct nfsdirent *nfsdirent;
+                       off_t offset, struct fuse_file_info *fi) {
+    struct nfsdir *nfsdir;
+    struct nfsdirent *nfsdirent;
 
-	int ret;
-	ret = nfs_opendir(nfs, path, &nfsdir);
-	if (ret < 0) {
-		return ret;
-	}
+    int ret;
+    ret = nfs_opendir(nfs, path, &nfsdir);
+    if (ret < 0) {
+        return ret;
+    }
 
-	char *name_buffer;
-	while (nfs_readdir(nfsdir)) != NULL) {
-		filler(buf, nfsdirent->name, NULL, 0);
-	}
-	nfs_closedir(nfs, nfsdir);
-	return 0;
+    char *name_buffer;
+    while (nfs_readdir(nfsdir)) != NULL) {
+        filler(buf, nfsdirent->name, NULL, 0);
+    }
+    nfs_closedir(nfs, nfsdir);
+    return 0;
 }
 
-static int nfs_open(const char *path, struct fuse_file_info *fi)
-{
-	int ret = 0;
-	file_handler *nfsfh;
+static int nfs_open(const char *path, struct fuse_file_info *fi) {
+    int ret = 0;
+    file_handler *nfsfh;
 
-	fi->fh = 0;
-	ret = nfs_open(server_ip, path, &nfsfh);
-	if (ret < 0) {
-		return ret;
-	}
+    fi->fh = 0;
+    ret = nfs_open(server_ip, path, &nfsfh);
+    if (ret < 0) {
+        return ret;
+    }
 
-	fi->fh = (uint64_t)nfsfh; //file handler returned by the server to identify call
-	return ret;
+    fi->fh = (uint64_t) nfsfh; //file handler returned by the server to identify call
+    return ret;
 }
 
 
-static int nfs_create(const char *path, mode_t mode, struct fuse_file_info *fi)
-{
-	int ret = 0;
-	struct nfsfh *nfsfh;
+static int nfs_create(const char *path, mode_t mode, struct fuse_file_info *fi) {
+    int ret = 0;
+    struct nfsfh *nfsfh;
 
-	ret = nfs_creat(nfs, path, mode, &nfsfh);
-	if (ret < 0) {
-		return ret;
-	}
+    ret = nfs_creat(nfs, path, mode, &nfsfh);
+    if (ret < 0) {
+        return ret;
+    }
 
-	fi->fh = (uint64_t)nfsfh;
+    fi->fh = (uint64_t) nfsfh;
 
-	return ret;
+    return ret;
 }
 
 static int nfs_read(const char *path, char *buf, size_t size,
-       off_t offset, struct fuse_file_info *fi)
-{
-	file_handler *nfsfh = (struct nfsfh *)fi->fh;
+                    off_t offset, struct fuse_file_info *fi) {
+    file_handler *nfsfh = (struct nfsfh *) fi->fh;
 
 
-	return nfs_read(server_ip, nfsfh, offset, size, buf);
+    return nfs_read(server_ip, nfsfh, offset, size, buf);
 }
 
 
 static int nfs_write(const char *path, const char *buf, size_t size,
-       off_t offset, struct fuse_file_info *fi)
-{
-	struct nfsfh *nfsfh = (struct nfsfh *)fi->fh;
+                     off_t offset, struct fuse_file_info *fi) {
+    struct nfsfh *nfsfh = (struct nfsfh *) fi->fh;
 
-        update_rpc_credentials();
+    update_rpc_credentials();
 
-	return nfs_pwrite(nfs, nfsfh, offset, size, discard_const(buf));
+    return nfs_pwrite(nfs, nfsfh, offset, size, discard_const(buf));
 }
 
 
-static int nfs_mkdir(const char *path, mode_t mode)
-{
-	int ret = 0;
+static int nfs_mkdir(const char *path, mode_t mode) {
+    int ret = 0;
 
-	ret = nfs_mkdir(nfs, path);
-	if (ret < 0) {
-		return ret;
-	}
-	ret = nfs_chmod(path);
-	if (ret < 0) {
-		return ret;
-	}
+    ret = nfs_mkdir(nfs, path);
+    if (ret < 0) {
+        return ret;
+    }
+    ret = nfs_chmod(path);
+    if (ret < 0) {
+        return ret;
+    }
 
-	return ret;
+    return ret;
 }
 
-static int nfs_rmdir(const char *path)
-{
-	update_rpc_credentials();
+static int nfs_rmdir(const char *path) {
+    update_rpc_credentials();
 
-	return nfs_rmdir(nfs, path);
+    return nfs_rmdir(nfs, path);
 }
 
- static int nfs_rename(const char *from, const char *to)
-{
+static int nfs_rename(const char *from, const char *to) {
 
-	return nfs_rename(nfs, from, to);
+    return nfs_rename(nfs, from, to);
 }
 
 static int nfs_fsync(const char *path, int isdatasync,
-			  struct fuse_file_info *fi)
-{
-	struct nfsfh *nfsfh = (struct nfsfh *)fi->fh;
+                     struct fuse_file_info *fi) {
+    struct nfsfh *nfsfh = (struct nfsfh *) fi->fh;
 
-	update_rpc_credentials();
+    update_rpc_credentials();
 
-	return nfs_fsync(nfs, nfsfh);
+    return nfs_fsync(nfs, nfsfh);
 }
 
 
 static struct fuse_operations nfs_oper = {
-	.create		= fuse_nfs_create,
-	.fsync		= fuse_nfs_fsync,
-	.mkdir		= fuse_nfs_mkdir,
-	.open		= fuse_nfs_open,
-	.read		= fuse_nfs_read,
-	.readdir	= fuse_nfs_readdir,
-	.rmdir		= fuse_nfs_rmdir,
-	.rename		= fuse_nfs_rename,
-	.write		= fuse_nfs_write,
+        .create        = fuse_nfs_create,
+        .fsync        = fuse_nfs_fsync,
+        .mkdir        = fuse_nfs_mkdir,
+        .open        = fuse_nfs_open,
+        .read        = fuse_nfs_read,
+        .readdir    = fuse_nfs_readdir,
+        .rmdir        = fuse_nfs_rmdir,
+        .rename        = fuse_nfs_rename,
+        .write        = fuse_nfs_write,
 };
 
- int main(int argc, char *argv[])
- {
- 	struct fuse_args args = FUSE_ARGS_INIT(argc, argv);
- 	struct fuse_cmdline_opts opts;
- 	struct stat stbuf;
+int main(int argc, char *argv[]) {
+    //communicate with server to set u_id
+    u_id = 0;
 
- 	if (fuse_parse_cmdline(&args, &opts) != 0)
- 		return 1;
+    if (argc < 2){
+    	perror("No server ip specified");
+        exit(1);
+    }
 
- 	if (!opts.mountpoint) {
- 		fprintf(stderr, "missing mountpoint parameter\n");
- 		return 1;
- 	}
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Problem in creating the socket");
+        exit(2);
+    }
 
- 	//communicate with server to set u_id
- 	u_id = 0;
- 	//initialize server ip
- 	strcpy(server_ip, argv[1]);
+    //Creation of the socket
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = inet_addr(argv[1]);
+    servaddr.sin_port = htons(SERV_PORT); //convert to big-endian order
 
- 	return fuse_main(argc, argv, &nfs_oper, NULL);
- }
+    //Connection of the client to the socket
+    if (connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) {
+        perror("Problem in connecting to the server");
+        exit(3);
+    }
+
+    return fuse_main(argc, argv, &nfs_oper, NULL);
+}
--- nfsClient.c
+++ nfsClient.c
@@ -83,17 +83,27 @@ void serialize_str(const char *x, my_buffer *b) {
     b->next += strlen(x) + 1;
 }
 
+
+void serialize_data(const char *data, my_buffer *b, size_t size) {
+    reserve_space(b, size);
+    memcpy(b->data + b->next, data, size);
+    //printf("%s\n", b->data + b->next);
+    b->next += size;
+}
+
 //deserialize str from next
-void deserialize_int(int *x, char *b, size_t offset) {
+void deserialize_int(int *x, char *b, size_t *offset) {
     int tmp;
-    memcpy(&tmp, b + offset, sizeof(int));
+    memcpy(&tmp, b + *offset, sizeof(int));
+    *offset += sizeof(int);
     *x = ntohl(tmp);
 }
 
 //deserialize str from next
-char *deserialize_str(char *b, size_t offset) {
-    char *ret = malloc(strlen(b + offset) + 1);//str will be null terminated
-    strcpy(ret, b + offset);
+char *deserialize_str(char *b, size_t *offset) {
+    char *ret = malloc(strlen(b + *offset) + 1);//str will be null terminated
+    strcpy(ret, b + *offset);
+    *offset += strlen(ret) + 1;
     printf("ret size: %d\n", strlen(ret));
     return ret;
 }
@@ -113,13 +123,95 @@ void get_tcp_response(int sockfd, char *recvbuffer) {
 
 }
 
+
+void *get_readdir_response(int sockfd, int *num_dir) {
+    char buffer[BUFFER_SIZE];
+    char *data_buffer;
+    size_t n = recv(sockfd, buffer, BUFFER_SIZE, 0); //initial read
+    if (n == 0) {
+        //error: server terminated prematurely
+        printf("The server terminated prematurely\n");
+        exit(4);
+    }
+    printf("WOW\n");
+    size_t offset = 0;
+    deserialize_int(num_dir, buffer, &offset);
+    int total_bytes;
+    deserialize_int(&total_bytes, buffer, &offset);
+    if (total_bytes > 0) {
+        data_buffer = malloc(total_bytes);
+        int read_bytes = n - offset;
+        memcpy(data_buffer, buffer + offset, read_bytes);
+        int rest_bytes = total_bytes - read_bytes;
+        offset = read_bytes;
+        printf("read: %d\n", read_bytes);
+        printf("rest: %d\n", rest_bytes);
+        while (rest_bytes > 0) {//buffer is full
+            printf("WOW4\n");
+            //deadlock when server failed
+            read_bytes = recv(sockfd, data_buffer + offset, rest_bytes, 0);
+            offset += read_bytes;
+            rest_bytes -= read_bytes;
+            printf("data_buffer: %s\n", data_buffer);
+            printf("read: %d\n", read_bytes);
+            printf("rest: %d\n", rest_bytes);
+        }
+    }
+    return data_buffer;
+}
+
+
+
+char *get_read_response(int sockfd, file_handler *fh) {
+    char buffer[BUFFER_SIZE];
+    char *data_buffer;
+    size_t n = recv(sockfd, buffer, BUFFER_SIZE, 0); //initial read
+    if (n == 0) {
+        //error: server terminated prematurely
+        printf("The server terminated prematurely\n");
+        exit(4);
+    }
+    printf("WOW\n");
+    int response_code;
+    size_t offset = 0;
+    deserialize_int(&response_code, buffer, &offset);
+    printf("WOW5\n");
+    fh->wc = deserialize_str(buffer, &offset);
+    if (response_code == 0) {
+        printf("WOW2\n");
+        int total_bytes;
+        deserialize_int(&total_bytes, buffer, &offset);
+        data_buffer = malloc(total_bytes);
+        int read_bytes = n - offset;
+        memcpy(data_buffer, buffer + offset, read_bytes);
+        printf("WOW3\n");
+        int rest_bytes = total_bytes - read_bytes;
+        offset = read_bytes;
+        printf("read: %d\n", read_bytes);
+        printf("rest: %d\n", rest_bytes);
+        while (rest_bytes > 0) {//buffer is full
+            printf("WOW4\n");
+            //deadlock when server failed
+            read_bytes = recv(sockfd, data_buffer + offset, rest_bytes, 0);
+            offset += read_bytes;
+            rest_bytes -= read_bytes;
+            printf("data_buffer: %s\n", data_buffer);
+            printf("read: %d\n", read_bytes);
+            printf("rest: %d\n", rest_bytes);
+        }
+
+    }
+
+    return data_buffer;
+}
+
 //break large packet into several small one
 void handle_send(int sockfd, my_buffer *arg_buffer) {
-    char sendbuffer[BUFFER_SIZE];
+    char sendbuffer[arg_buffer->next];
     memset(sendbuffer, 0, sizeof(sendbuffer));
     memcpy(sendbuffer, arg_buffer->data, arg_buffer->next);
     if (arg_buffer->size <= BUFFER_SIZE) {
-        send(sockfd, sendbuffer, arg_buffer->size, 0);
+        send(sockfd, sendbuffer, arg_buffer->next, 0);
     } else {
 
     }
@@ -135,7 +227,7 @@ void print_buffer(my_buffer *arg_buffer) {
 
 int nfs_open_internal(int sockfd, const char *path, file_handler **fh, const char *command) {
     //sed a path to server, get a file handler
-    int cid = 1;
+    int cid = 0;
     my_buffer *arg_buffer = new_buffer();
     serialize_int(cid, arg_buffer);
     print_buffer(arg_buffer);
@@ -165,16 +257,14 @@ int nfs_open_internal(int sockfd, const char *path, file_handler **fh, const cha
     file_handler *my_fh = malloc(sizeof(file_handler));
     int f_id;
     size_t offset = 0;
-    deserialize_int(&f_id, recvbuffer, offset);
-    offset += sizeof(f_id);
-
+    deserialize_int(&f_id, recvbuffer, &offset);
     if (f_id < 0) return -1;
 
     my_fh->file_id = f_id;
     printf("file_id: %d\n", my_fh->file_id);
     my_fh->offset = 0;
     printf("offset: %d\n", my_fh->offset);
-    my_fh->wc = deserialize_str(recvbuffer,offset);
+    my_fh->wc = deserialize_str(recvbuffer, &offset);
     printf("wc: %s\n", my_fh->wc);
 
 
@@ -223,7 +313,8 @@ int nfs_rmdir(int sockfd, const char *path) {
 
 
     int response_code;
-    deserialize_int(&response_code, recvbuffer, 0);
+    size_t offset = 0;
+    deserialize_int(&response_code, recvbuffer, &offset);
     if (response_code < 0) {
         printf("delete failed\n");
         return -1;
@@ -231,6 +322,170 @@ int nfs_rmdir(int sockfd, const char *path) {
         printf("delete success\n");
 
     }
+    free(arg_buffer);
+    return 0;
+}
+
+
+int nfs_read(int sockfd, file_handler *nfsfh, size_t offset, size_t size, char *buf) {
+    int cid = 2;
+    printf("===========start read=========\n");
+    my_buffer *arg_buffer = new_buffer();
+    serialize_int(cid, arg_buffer);
+    serialize_str("READ", arg_buffer);
+    print_buffer(arg_buffer);
+    serialize_int(nfsfh->file_id, arg_buffer);
+    serialize_int(offset, arg_buffer);
+    serialize_int(size, arg_buffer);
+    serialize_str(nfsfh->wc, arg_buffer);
+    print_buffer(arg_buffer);
+    serialize_char('\n', arg_buffer);
+    print_buffer(arg_buffer);
+
+
+    printf("HERE1\n");
+
+    handle_send(sockfd, arg_buffer);
+
+    printf("HERE2\n");
+
+    char *data = get_read_response(sockfd, nfsfh);
+
+    printf("HERE3\n");
+
+    strcpy(buf, data);
+
+    free(data);
+
+    printf("data: %s\n", buf);
+
+    free(arg_buffer);
+    return 0;
+}
+
+
+int nfs_write(int sockfd, file_handler *nfsfh, size_t offset, size_t size, char *buf) {
+    int cid = 2;
+    printf("===========start write=========\n");
+    my_buffer *arg_buffer = new_buffer();
+    serialize_int(cid, arg_buffer);
+    serialize_str("WRITE", arg_buffer);
+    print_buffer(arg_buffer);
+    serialize_int(nfsfh->file_id, arg_buffer);
+    serialize_int(offset, arg_buffer);
+    serialize_int(size, arg_buffer);
+    serialize_str(nfsfh->wc, arg_buffer);
+    serialize_data(buf, arg_buffer, size);
+    serialize_char('\n', arg_buffer);
+    print_buffer(arg_buffer);
+
+
+    char recvbuffer[BUFFER_SIZE]; //need to receive a file handler
+
+    printf("HERE1\n");
+
+    handle_send(sockfd, arg_buffer);
+
+    printf("HERE2\n");
+
+    get_tcp_response(sockfd, recvbuffer);
+
+    printf("HERE3\n");
+
+
+    int response_code;
+    size_t my_offset = 0;
+    deserialize_int(&response_code, recvbuffer, &my_offset);
+    nfsfh->wc = deserialize_str(recvbuffer, &my_offset);
+    printf("wc: %s\n", nfsfh->wc);
+
+    if (response_code < 0) {
+        printf("write failed\n");
+        return -1;
+    } else {
+        printf("write success\n");
+
+    }
+    return 0;
+}
+
+int nfs_fsync(int sockfd, file_handler *nfsfh) {
+
+    int cid = 2;
+    printf("===========start fsync=========\n");
+    my_buffer *arg_buffer = new_buffer();
+    serialize_int(cid, arg_buffer);
+    serialize_str("COMMIT", arg_buffer);
+    print_buffer(arg_buffer);
+    serialize_int(nfsfh->file_id, arg_buffer);
+    serialize_str(nfsfh->wc, arg_buffer);
+    print_buffer(arg_buffer);
+    serialize_char('\n', arg_buffer);
+    print_buffer(arg_buffer);
+
+    char recvbuffer[BUFFER_SIZE]; //need to receive a file handler
+
+
+    printf("HERE1\n");
+
+    handle_send(sockfd, arg_buffer);
+
+    printf("HERE2\n");
+
+    get_tcp_response(sockfd, recvbuffer);
+
+    printf("HERE3\n");
+
+
+    int response_code;
+    size_t my_offset = 0;
+    deserialize_int(&response_code, recvbuffer, &my_offset);
+    nfsfh->wc = deserialize_str(recvbuffer, &my_offset);
+    printf("wc: %s\n", nfsfh->wc);
+
+    if (response_code < 0) {
+        printf("fsync failed\n");
+        return -1;
+    } else {
+        printf("fsync success\n");
+
+    }
+
+    return 0;
+}
+
+
+
+int nfs_read_dir(int sockfd, const char *path, char ***dirs) {
+
+    int cid = 4;
+    my_buffer *arg_buffer = new_buffer();
+    serialize_int(cid, arg_buffer);
+    serialize_str("READDIR", arg_buffer);
+    serialize_str(path, arg_buffer);
+    serialize_char('\n', arg_buffer);
+
+    char recvbuffer[BUFFER_SIZE]; //need to receive a file handler
+    printf("HERE1\n");
+    handle_send(sockfd, arg_buffer);
+    printf("HERE2\n");
+    get_tcp_response(sockfd, recvbuffer);
+    printf("HERE3\n");
+
+    int rcv_num_dir;
+    size_t offset = 0;
+    deserialize_int(&rcv_num_dir, recvbuffer, &offset);
+
+    char **all_dirs = malloc(sizeof(*all_dirs) * rcv_num_dir);
+
+    int i;
+    for (i = 0; i < rcv_num_dir; ++i) {
+        int path_len = 0;
+        all_dirs[i] = deserialize_str(recvbuffer, &offset);
+        printf("%s\n", all_dirs[i]);
+    }
+
+    *dirs = all_dirs;
     free(arg_buffer);
     return 0;
 }
@@ -264,13 +519,28 @@ int main(int argc, char *argv[]) {
         exit(3);
     }
 
-    printf("HERE\n");
+//    file_handler *fh;
+//    //nfs_create(sockfd, argv[2], &fh);
+//    nfs_open(sockfd, argv[2], &fh);
+//    // nfs_mkdir(sockfd, argv[2]);
+//    //nfs_rmdir(sockfd, argv[2]);
+//    //char *buf = malloc(10*BUFFER_SIZE);
+//    //nfs_read(sockfd, fh, 0, 3*BUFFER_SIZE, buf);
+//    char *to_write = "ABCDEFG";
+//
+//    int i=0;
+//    size_t offset = 0;
+//    for (i=0; i < 100; i++){
+//        printf(".");
+//        nfs_write(sockfd, fh, offset, strlen(to_write), to_write);
+//        offset += strlen(to_write);
+//    }
+//    //nfs_write(sockfd, fh, 0, strlen(to_write), to_write);
+//    nfs_fsync(sockfd, fh);
 
-    file_handler *fh;
-    //nfs_create(sockfd, argv[2], &fh);
-    //nfs_open(sockfd, argv[2], &fh);
-    // nfs_mkdir(sockfd, argv[2]);
-    nfs_rmdir(sockfd, argv[2]);
+    char **dirs;
+    nfs_read_dir(sockfd, argv[2], &dirs);
+
     return 0;
 }
 
