1d2389cca388823e0604226361ee03b0966e9980
NerdonblooR <njbbtan@gmail.com>
2017-02-22 15:18:58 -0500

    mod fuse

--- my_fuse_nfs.c
+++ my_fuse_nfs.c
@@ -578,10 +578,10 @@ int main(int argc, char *argv[]) {
         exit(2);
     }
 
-    //Creation of the socket
+    //Creation of the socket:
     memset(&servaddr, 0, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = inet_addr(argv[2]);
+    servaddr.sin_addr.s_addr = inet_addr("192.168.0.16");
     servaddr.sin_port = htons(SERV_PORT); //convert to big-endian order
 
     //Connection of the client to the socket
